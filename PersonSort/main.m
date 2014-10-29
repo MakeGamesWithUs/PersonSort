443ae3028e51275fd40ba54e127c99b375e8c914
Ben-G <Benjamin.Encz@gmail.com>
2014-10-29 14:34:13 +0100

    add compare method as third benchmark

--- PersonSort/main.m
+++ PersonSort/main.m
@@ -11,7 +11,7 @@
 
 extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
 
-static const NSInteger kPersonCount = 2000;
+static const NSInteger kPersonCount = 10000;
 
 NSArray *sortPersonsBubbleSort(NSArray *persons);
 NSArray *sortPersonsBucketSort(NSArray *persons);
@@ -24,11 +24,11 @@ int main(int argc, const char * argv[]) {
             persons[i] = [Person randomPerson];
         }
         
-        __block NSArray *sortedBubble = nil;
-        
-        uint64_t t = dispatch_benchmark(3, ^{
-            sortedBubble = sortPersonsBubbleSort([persons copy]);
-        });
+//        __block NSArray *sortedBubble = nil;
+//        
+//        uint64_t t = dispatch_benchmark(3, ^{
+//            sortedBubble = sortPersonsBubbleSort([persons copy]);
+//        });
         
         __block NSArray *sortedBucket = nil;
 
@@ -36,14 +36,32 @@ int main(int argc, const char * argv[]) {
             sortedBucket = sortPersonsBucketSort([persons copy]);
         });
         
-        if (!(sortedBubble[333] == sortedBucket[333])) {
-            @throw [NSException exceptionWithName:@"Incorrect sorting" reason:@"Both algorithms should sort the same way!" userInfo:nil];
-        }
+        __block NSArray *sortedComparator = nil;
         
-        NSLog(@"[Bubble Sort] Avg. Runtime: %llu ms", t/1000000);
+        uint64_t tComparator = dispatch_benchmark(3, ^{
+            sortedComparator = [[persons copy] sortedArrayUsingComparator:^NSComparisonResult(Person *person1, Person *person2) {
+                if (person1.age < person2.age) {
+                    return NSOrderedDescending;
+                } else if (person1.age > person2.age) {
+                    return NSOrderedAscending;
+                } else {
+                    NSComparisonResult lastNameCompare = [person1.lastName compare:person2.lastName];
+                    if (lastNameCompare != NSOrderedSame) {
+                        return lastNameCompare;
+                    } else {
+                        return [person1.firstName compare:person2.firstName];
+                    }
+                }
+            }];
+        });
+        
+//        if (!( (sortedBubble[333] == sortedBucket[333]) && (sortedComparator[333] && sortedBucket[333]))) {
+//            @throw [NSException exceptionWithName:@"Incorrect sorting" reason:@"Both algorithms should sort the same way!" userInfo:nil];
+//        }
+//        
+//        NSLog(@"[Bubble Sort] Avg. Runtime: %llu ms", t/1000000);
         NSLog(@"[Bucket Sort] Avg. Runtime: %llu ms", tBucket/1000000);
-        
-        
+        NSLog(@"[Comparator Sort] Avg. Runtime: %llu ms", tComparator/1000000);
     }
     return 0;
 }
