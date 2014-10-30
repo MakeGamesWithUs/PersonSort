e057b9cc3ca2843c918cdc0b0a2ab71ab333d645
Ben-G <Benjamin.Encz@gmail.com>
2014-10-30 15:54:04 +0100

    remmove solution

--- PersonSort/BubbleSort.m
+++ PersonSort/BubbleSort.m
@@ -12,46 +12,7 @@
 @implementation BubbleSort
 
 + (NSArray *)sort:(NSArray *)persons {
-    NSMutableArray *sortedPersons = [persons mutableCopy];
-    
-    BOOL swapped = YES;
-    
-    while (swapped) {
-        swapped = NO;
-        
-        for (int i = 0; i+1 < [sortedPersons count]; i++) {
-            BOOL needsSwap = NO;
-            
-            if ([sortedPersons[i] age] > [sortedPersons[i+1] age]) {
-                // swap the elements
-                needsSwap = YES;
-            } else if ([sortedPersons[i] age] == [sortedPersons[i+1] age]) {
-                NSComparisonResult lastNameCompare = [[sortedPersons[i] lastName] compare:[sortedPersons[i+1] lastName]];
-                
-                if (lastNameCompare == NSOrderedDescending) {
-                    needsSwap = YES;
-                } else if (lastNameCompare == NSOrderedSame) {
-                    NSComparisonResult firstNameCompare = [[sortedPersons[i] firstName] compare:[sortedPersons[i+1] firstName]];
-                    if (firstNameCompare == NSOrderedDescending) {
-                        needsSwap = YES;
-                    }
-                }
-            }
-            
-            if (needsSwap) {
-                Person *temp = sortedPersons[i];
-                sortedPersons[i] = sortedPersons[i+1];
-                sortedPersons[i+1] = temp;
-                swapped = YES;
-            }
-        }
-        
-        if (!swapped) {
-            // if there was an entire round without swapping any elements, we are done sorting
-            return sortedPersons;
-        }
-    }
-    
-    return sortedPersons;}
+    return nil;
+}
 
 @end
