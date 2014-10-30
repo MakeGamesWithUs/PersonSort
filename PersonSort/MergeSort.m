f46fabb796a630446273c09e73747677cb5e5fc1
Ben-G <Benjamin.Encz@gmail.com>
2014-10-30 12:49:00 +0100

    actual merge sort implementation

--- PersonSort/MergeSort.m
+++ PersonSort/MergeSort.m
@@ -12,67 +12,70 @@
 @implementation MergeSort
 
 + (NSArray *)sort:(NSArray *)persons {
+    if ([persons count] <= 1) {
+        return persons;
+    }
+    
     NSRange rangeFirstHalf = NSMakeRange(0, ([persons count] / 2));
     NSRange rangeSecondHalf = NSMakeRange([persons count] / 2, [persons count] - rangeFirstHalf.length);
     
     NSArray *arrayFirstHalf = [persons subarrayWithRange:rangeFirstHalf];
     NSArray *arraySecondHalf = [persons subarrayWithRange:rangeSecondHalf];
     
-    if ([arrayFirstHalf count] > 1 || [arraySecondHalf count] > 1) {
-        NSArray *leftHalf = [self sort:arrayFirstHalf];
-        NSArray *rightHalf = [self sort:arraySecondHalf];
-        
-        return [self mergeArrayOne:leftHalf arrayTwo:rightHalf];
-    } else {
-        return [self mergeArrayOne:arrayFirstHalf arrayTwo:arraySecondHalf];
-    }
+    NSArray *leftHalf = [self sort:arrayFirstHalf];
+    NSArray *rightHalf = [self sort:arraySecondHalf];
+    
+    return [self mergeArrayOne:leftHalf arrayTwo:rightHalf];
 }
 
-+ (NSArray *)mergeArrayOne:(NSArray *)arrayFirstHalf arrayTwo:(NSArray *)arraySecondHalf {
++ (NSArray *)mergeArrayOne:(NSArray *)arrayOne arrayTwo:(NSArray *)arrayTwo {
     // merge
-    
-    if ([arrayFirstHalf count] == 0) {
-        return arraySecondHalf;
-    } else if ([arraySecondHalf count] == 0) {
-        return arrayFirstHalf;
-    }
+    NSMutableArray *arrayFirstHalf = [arrayOne mutableCopy];
+    NSMutableArray *arraySecondHalf = [arrayTwo mutableCopy];
     
     NSMutableArray *mergedArray = [NSMutableArray array];
-    [mergedArray addObjectsFromArray:arrayFirstHalf];
-    
-    for (int i = 0; i < [arraySecondHalf count]; i++) {
-        Person *selectedPerson = arraySecondHalf[i];
+
+    while ([arrayFirstHalf count] > 0 && [arraySecondHalf count] > 0) {
+        BOOL leftSmaller = NO;
+        Person *personLeft = arrayFirstHalf[0];
+        Person *personRight = arraySecondHalf[0];
         
-        NSInteger targetIndex = [mergedArray count];
-        BOOL smaller = YES;
-        while (smaller) {
-            smaller = NO;
-            Person *otherPerson = mergedArray[targetIndex-1];
+        if (personLeft.age < personRight.age) {
+            leftSmaller = YES;
+        } else if (personLeft.age == personRight.age) {
+            NSComparisonResult lastNameCompare = [[personLeft lastName] compare:[personRight lastName]];
             
-            if (selectedPerson.age < otherPerson.age) {
-                smaller = YES;
-            } else if (selectedPerson.age == otherPerson.age) {
-                NSComparisonResult lastNameCompare = [[otherPerson lastName] compare:[selectedPerson lastName]];
-                
-                if (lastNameCompare == NSOrderedDescending) {
-                    smaller = YES;
-                } else if (lastNameCompare == NSOrderedSame) {
-                    NSComparisonResult firstNameCompare = [[otherPerson firstName] compare:[selectedPerson firstName]];
-                    if (firstNameCompare == NSOrderedDescending) {
-                        smaller = YES;
-                    }
+            if (lastNameCompare == NSOrderedDescending) {
+                leftSmaller = NO;
+            } else if (lastNameCompare == NSOrderedAscending) {
+                leftSmaller = YES;
+            } else if (lastNameCompare == NSOrderedSame) {
+                NSComparisonResult firstNameCompare = [[personLeft firstName] compare:[personRight firstName]];
+                if (firstNameCompare == NSOrderedDescending) {
+                    leftSmaller = NO;
+                } else if (firstNameCompare == NSOrderedDescending) {
+                    leftSmaller = YES;
                 }
             }
-            
-            if (smaller && targetIndex > 0) {
-                targetIndex --;
-            }
-            
-            if (!smaller || targetIndex == 0) {
-                [mergedArray insertObject:selectedPerson atIndex:targetIndex];
-                break;
-            }
         }
+        
+        if (leftSmaller) {
+            [mergedArray addObject:arrayFirstHalf[0]];
+            [arrayFirstHalf removeObjectAtIndex:0];
+        } else {
+            [mergedArray addObject:arraySecondHalf[0]];
+            [arraySecondHalf removeObjectAtIndex:0];
+        }
+    }
+    
+    while ([arrayFirstHalf count] > 0) {
+        [mergedArray addObject:arrayFirstHalf[0]];
+        [arrayFirstHalf removeObjectAtIndex:0];
+    }
+    
+    while ([arraySecondHalf count] > 0) {
+        [mergedArray addObject:arraySecondHalf[0]];
+        [arraySecondHalf removeObjectAtIndex:0];
     }
     
     return mergedArray;
